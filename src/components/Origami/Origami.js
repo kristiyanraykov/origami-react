999c9a2efdce5b4e2055de939a5e85f8f9d7ef73
Kristiyan Raykov <78812302+kristiyanraykov@users.noreply.github.com>
2021-08-01 20:28:42 +0300

    Add files via upload

--- src/components/Origami/Origami.js
+++ src/components/Origami/Origami.js
@@ -1,9 +1,20 @@
+import styles from './Origami.module.css'
+
 const Origami = ({
-    description
+    description,
+    author
 }) => {
     return (
-        <div>
-            {description}
+        <div className={styles.container}>
+            <p className={styles.description}>
+                {description}
+            </p>
+            <div className={styles['author-wrapper']}>
+                <span className={styles.author}>
+                    <small>Author:</small>
+                    {author}
+                </span>
+            </div>
         </div>
     )
 }
